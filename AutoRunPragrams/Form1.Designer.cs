4642a8e5f0ad6f8bee38bbf2aee979edb0a22956
shaon <shaon@JASON-SURFACE>
2016-07-23 00:14:16 +0800

    添加项目文件。

---
+++ AutoRunPragrams/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace AutoRunPragrams
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listboxPrograms = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listboxPrograms
+            // 
+            this.listboxPrograms.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listboxPrograms.FormattingEnabled = true;
+            this.listboxPrograms.ItemHeight = 24;
+            this.listboxPrograms.Location = new System.Drawing.Point(0, 0);
+            this.listboxPrograms.Name = "listboxPrograms";
+            this.listboxPrograms.Size = new System.Drawing.Size(1117, 764);
+            this.listboxPrograms.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1117, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(901, 22);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(192, 57);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "删除选中文件";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(24, 22);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(192, 57);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "添加启动文件";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listboxPrograms);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1117, 764);
+            this.panel2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 864);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "开机启动程序";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listboxPrograms;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ AutoRunPragrams/Form1.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+using System.Web.Script.Serialization;//引入命名空间,提供用于自定义序列化行为的扩展性功能
+
+
+namespace AutoRunPragrams
+{
+    public partial class Form1 : Form
+    {
+        string ConfigFile;
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ConfigFile = Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments) + "/AutoRunPrograms.cfg";
+            List<FileItem> list= Read();
+            try
+            {
+                //添加启动
+                RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+                key.SetValue("自动启动", Application.ExecutablePath.ToString());
+
+                if (list != null && list.Count > 0)
+                {
+                    Thread th = new Thread(delegate ()
+                    {
+                        Thread.Sleep(1000 * 60 * 2);
+                        
+                        foreach (FileItem item in list)
+                        {
+                            Process.Start(item.name);
+                        }
+
+                        Thread.Sleep(1000 * 60 * 2);
+                        Environment.Exit(0);
+                    });
+                    th.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }           
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            if(dialog.ShowDialog()== DialogResult.OK)
+            {
+                listboxPrograms.Items.Add(dialog.FileName);
+            }
+            Save();
+        }
+
+
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listboxPrograms.SelectedIndex >= 0)
+            {
+                listboxPrograms.Items.RemoveAt(listboxPrograms.SelectedIndex);
+            }
+            Save();
+        }
+
+        //保存列表
+        void Save() {
+            List<FileItem> list = new List<FileItem>();
+            foreach(string s in listboxPrograms.Items)
+            {
+                list.Add(new FileItem { name = s });
+            }
+
+            //AJAX的应用程序提供序列化和反序列化功能
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            //将对象转换为JSON格式字符串 
+            var json = js.Serialize(list);
+            File.WriteAllText(ConfigFile, json);
+        }
+
+        //读取
+        List<FileItem> Read() {
+            try
+            {
+                string cfg = File.ReadAllText(ConfigFile);
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                //将对象转换为JSON格式字符串 
+                List<FileItem> list = js.Deserialize<List<FileItem>>(cfg);
+                foreach (FileItem item in list)
+                {
+                    listboxPrograms.Items.Add(item.name);
+                }
+                return list;
+            }catch(Exception ex)
+            {
+                return null;
+            }
+            
+        }
+    }
+}
